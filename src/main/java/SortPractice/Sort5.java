bddf922161e09007b0f34ffdf7d2e0875719c4c5
shahar <jkunuy27@gmail.com>
2021-12-25 14:23:17 +0200

    practice exercises from the work book

--- src/main/java/SortPractice/Sort5.java
+++ src/main/java/SortPractice/Sort5.java
@@ -4,14 +4,16 @@ public class Sort5 {
     public static void main(String[] args) {
         final int ROWS = 4, COLS = 4;
         int[][] mat = new int[ROWS][COLS];
-        /*
+
         System.out.println("targil 5: bubble sort");
         randomValues(mat);
         printMatrix(mat);
         bubbleSortMatrix(mat, mat.length, mat[0].length);
         System.out.println();
         printMatrix(mat);
-        */
+
+        System.out.println();
+
         System.out.println("targil 6: selection sort");
         randomValues(mat);
         printMatrix(mat);
@@ -47,7 +49,6 @@ public class Sort5 {
                     temp = mat[index / cols][index % cols];
                     mat[index / cols][index % cols] = mat[(index + 1) / cols][(index + 1) % cols];
                     mat[(index + 1) / cols][(index + 1) % cols] = temp;
-                    //swap(mat[index / cols][index % cols],mat[(index + 1) / cols][(index + 1) % cols]);
                 }
             }
         }
@@ -56,26 +57,19 @@ public class Sort5 {
     public static void selectionSortMatrix(int[][] mat, int rows, int cols) {
         int temp, size = rows * cols, minRow, minCol;
         for (int i = 0; i < size; i++) {
-            minRow = 1;
-            minCol = 1;
+            minRow = i/cols;
+            minCol = i%cols;
             for (int index = i + 1; index < size; index++) {
                 if (mat[index / cols][index % cols] < mat[minRow][minCol]) {
                     minRow = index / cols;
-                    minRow = index % cols;
+                    minCol = index % cols;
                 }
             }
-            if (minRow != 1 || minCol != 1) {
+            if (minRow != i/cols|| minCol != i%cols) {
                 temp = mat[i / cols][i % cols];
                 mat[i / cols][i % cols] = mat[minRow][minCol];
                 mat[minRow][minCol] = temp;
-                //swap(mat[i / cols][i % cols],mat[minRow][minCol]);
             }
         }
-    }
-
-    public static void swap(int bigNumber, int smallNumber){
-        int temp=bigNumber;
-        bigNumber=smallNumber;
-        smallNumber=temp;
     }
 }
