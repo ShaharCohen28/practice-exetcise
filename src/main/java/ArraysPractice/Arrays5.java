1e3addf36b7756e286fec320d6f7fd2cd1a73d7f
shahar <jkunuy27@gmail.com>
2022-01-02 19:11:12 +0200

    practice exercises from the work book

--- src/main/java/ArraysPractice/Arrays5.java
+++ src/main/java/ArraysPractice/Arrays5.java
@@ -4,35 +4,33 @@ import java.util.Scanner;
 
 public class Arrays5 {
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
         final int ARRAY_LENGTH=5;
         int[] arr1=new int[ARRAY_LENGTH], arr2= new int[ARRAY_LENGTH];
         boolean isOK=true;
 
-        System.out.println("Please enter " + ARRAY_LENGTH + " numbers to the first array");
-        for (int i=0; i<ARRAY_LENGTH; i++){
-            arr1[i]=scanner.nextInt();
-        }
-        System.out.println("Please enter " + ARRAY_LENGTH + " numbers to the second array");
-        for (int i=0; i<ARRAY_LENGTH; i++){
-            arr2[i]=scanner.nextInt();
-        }
-
+        initiateArray(arr1);
+        initiateArray(arr2);
         for(int i=0; i<ARRAY_LENGTH; i++){
             if(arr1[i]!=arr2[(i+1)%ARRAY_LENGTH]){
                 isOK=false;
                 break;
             }
         }
+        System.out.println("the numbers in array 2 are " + (isOK?"":"not ") + "deflected by one place");
+    }
 
-        for (int numbers:arr1){
+    public static void initiateArray(int[] arr){
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Please enter " + arr.length + " numbers to the array");
+        for (int i=0; i< arr.length; i++){
+            arr[i]=scanner.nextInt();
+        }
+    }
+
+    public static void printArray(int[] arr){
+        for (int numbers:arr){
             System.out.print(numbers + ", ");
         }
         System.out.println();
-        for (int numbers:arr2){
-            System.out.print(numbers + ", ");
-        }
-        System.out.println();
-        System.out.println("the numbers in array 2 are " + (isOK?"":" not") + " deflected by one place");
     }
 }
