c24fb14331866eceb7fb43320df01edf684705f2
shahar <jkunuy27@gmail.com>
2022-01-03 09:23:00 +0200

    practice exercises from the work book

--- src/main/java/ArraysPractice/Arrays6.java
+++ src/main/java/ArraysPractice/Arrays6.java
@@ -4,36 +4,37 @@ import java.util.Scanner;
 
 public class Arrays6 {
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
-        char[] chArr=new char[10];
-        boolean isCaps=false;
+        Scanner scanner = new Scanner(System.in);
+        char[] chArr = new char[10];
+        boolean isCaps = false;
 
-        System.out.println("Please enter " + chArr.length + " letters, 2 must be capital " +
-                "and the rest must be lower-case");
-        for (int i=0; i< chArr.length; i++){
-            chArr[i]=scanner.next().charAt(0);
-        }
-        for (int i=0; i< chArr.length; i++){
-            System.out.print(chArr[i]+ ", ");
-        }
-        System.out.println();
-
-        for (int i=0; i< chArr.length; i++){
-            if((chArr[i]>='A'&&chArr[i]<='Z')&&!isCaps){
-                isCaps=true;
+        initiateArray(chArr);
+        printArray(chArr);
+        for (int i = 0; i < chArr.length; i++) {
+            if (chArr[i] >= 'A' && chArr[i] <= 'Z') {
+                isCaps = !isCaps;
                 continue;
             }
-            if(isCaps&&!(chArr[i]>='A'&&chArr[i]<='Z')){
-                chArr[i]-=32;
-            }else{
-                isCaps=false;
+            if (isCaps) {
+                chArr[i] -= 32;
             }
         }
+        printArray(chArr);
+    }
 
-        for (int i=0; i< chArr.length; i++){
-            System.out.print(chArr[i]+ ", ");
+    public static void initiateArray(char[] arr) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter " + arr.length + " letters, 2 must be capital " +
+                "and the rest must be lower-case");
+        for (int index = 0; index < arr.length; index++) {
+            arr[index] = scanner.next().charAt(0);
+        }
+    }
+
+    public static void printArray(char[] arr) {
+        for (char letters : arr) {
+            System.out.print(letters + ", ");
         }
         System.out.println();
-
     }
 }
