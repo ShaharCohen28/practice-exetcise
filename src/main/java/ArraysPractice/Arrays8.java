7659cb0223eee7af7c61595ae27348cf0c265476
shahar <jkunuy27@gmail.com>
2022-01-03 08:50:59 +0200

    practice exercises from the work book

--- src/main/java/ArraysPractice/Arrays8.java
+++ src/main/java/ArraysPractice/Arrays8.java
@@ -4,29 +4,45 @@ import java.util.Scanner;
 
 public class Arrays8 {
     public static void main(String[] args) {
-        Scanner scanner= new Scanner(System.in);
-        final int ARRAY_SIZE=6;
-        int[] arr= new int[ARRAY_SIZE];
+
+        final int ARRAY_SIZE = 6;
+        int[] arr = new int[ARRAY_SIZE];
         int reverseValue, temp;
-        boolean isMirror=true;
+        boolean isMirror = true;
 
-        System.out.println("Please enter " + ARRAY_SIZE + " numbers for the first array");
-        for(int i=0; i<ARRAY_SIZE; i++){
-            arr[i]= scanner.nextInt();
-        }
-
-        for (int i=0; i<ARRAY_SIZE/2; i++){
-            reverseValue=0;
-            temp=arr[i];
-            while (temp>0){
-                reverseValue=10*reverseValue+temp%10;
-                temp/=10;
-            }
-            if(reverseValue!=arr[ARRAY_SIZE-1-i]){
-                isMirror=false;
+        initiateArray(arr);
+        for (int i = 0; i < ARRAY_SIZE / 2; i++) {
+            if (reverseValue(arr[i]) != arr[ARRAY_SIZE - 1 - i]) {
+                isMirror = false;
                 break;
             }
         }
-        System.out.println(isMirror?"Mirror arrays":"Not mirror arrays");
+        printArray(arr);
+        System.out.println(isMirror ? "Mirror arrays" : "Not mirror arrays");
+    }
+
+    public static void initiateArray(int[] arr) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter " + arr.length + " numbers for the first array");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scanner.nextInt();
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int number : arr) {
+            System.out.print(number + " ");
+        }
+        System.out.println();
+    }
+
+    public static int reverseValue(int number) {
+        int reverseValue = 0;
+        int temp = number;
+        while (temp > 0) {
+            reverseValue = 10 * reverseValue + temp % 10;
+            temp /= 10;
+        }
+        return reverseValue;
     }
 }
