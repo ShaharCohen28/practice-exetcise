97f61686791242717ecbc6601487a84af3fcaafd
shahar <jkunuy27@gmail.com>
2022-01-05 08:33:56 +0200

    practice exercises from the work book

--- src/main/java/exercise/Test.java
+++ src/main/java/exercise/Test.java
@@ -1,9 +1,12 @@
 package exercise;
 
+import java.util.Scanner;
+
 public class Test {
     public static void main(String[] args) {
-        Company.getInstance("company :)");
+        Company company=Company.getInstance("company :)");
         Employee[] employees = new Employee[10];
+        /*
         employees[0] = new Secretary("csdc", 23424, "grgreg");
         employees[1] = new Secretary("cds", 4242, " ddcs");
         employees[2] = new Manager("Sfsdfs", 223453, "stst");
@@ -14,6 +17,11 @@ public class Test {
         employees[7] = new Director("ssfds", 5433453, "rsfsvs", "rwfrww");
         employees[8] = new Engineer("fdgds", 3424, "dfgeg");
         employees[9] = new Engineer("ssfsf", 43242, "dsfsf");
+         */
+
+        for (int index = 0; index <employees.length ; index++) {
+            addEmployee(employees,index);
+        }
 
         printEmployeesDetails(employees);
         printAverageSalary(employees);
@@ -49,6 +57,62 @@ public class Test {
         } catch (ArithmeticException err) {
             System.out.println("No managers");
         }
+
+    }
+
+    public static void addEmployee(Employee[] employees, int index){
+        Scanner input=new Scanner(System.in);
+        String name, secondaryOption, directorOption;
+        double salary;
+        int option;
+        boolean isOK=false;
+
+        System.out.println("Please enter employee name");
+        name= input.next();
+        System.out.println("Please enter employee's salary");
+        salary=input.nextDouble();
+        System.out.println("1- add regular employee" +
+                "\n2- add secretary" +
+                "\n3- add engineer" +
+                "\n4- add manager" +
+                "\n5- add director");
+        option= input.nextInt();
+        do{
+            switch (option){
+                case 1:
+                    employees[index]=new Employee(name,salary);
+                    isOK=true;
+                    break;
+                case 2:
+                    System.out.println("Please enter secretary's  office");
+                    secondaryOption=input.next();
+                    employees[index]=new Secretary(name,salary,secondaryOption);
+                    isOK=true;
+                    break;
+                case 3:
+                    System.out.println("Please enter engineer's speciality");
+                    secondaryOption=input.next();
+                    employees[index]=new Engineer(name,salary,secondaryOption);
+                    isOK=true;
+                    break;
+                case 4:
+                    System.out.println("Please enter manager's department");
+                    secondaryOption=input.next();
+                    employees[index]=new Manager(name,salary,secondaryOption);
+                    isOK=true;
+                    break;
+                case 5:
+                    System.out.println("Please enter director's department");
+                    secondaryOption=input.next();
+                    System.out.println("Please enter director's group");
+                    directorOption=input.next();
+                    employees[index]=new Director(name,salary,secondaryOption, directorOption);
+                    isOK=true;
+                    break;
+                default:
+                    System.out.println("Invalid input, try again");
+            }
+        }while(!isOK);
 
     }
 }
