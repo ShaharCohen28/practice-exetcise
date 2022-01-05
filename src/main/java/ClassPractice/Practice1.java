a394eca25346e75ac6b8137cb2923aa77d34b71e
shahar <jkunuy27@gmail.com>
2022-01-05 17:53:56 +0200

    practice exercises from the work book

--- src/main/java/ClassPractice/Practice1.java
+++ src/main/java/ClassPractice/Practice1.java
@@ -8,18 +8,14 @@ public class Practice1 {
         final int ARRAY_SIZE = 15;
         int[] arr1 = randomValues(ARRAY_SIZE);
         int[] arr2 = randomValues(ARRAY_SIZE);
-        int[] arr3 = new int[ARRAY_SIZE];
+        int[] arr3;
         int number;
+
         printArray(arr1);
         printArray(arr2);
-        /*
         System.out.println("Please enter a number of partition");
         number = scanner.nextInt();
-        System.out.println("The max number with the length " + number + " is " + maxNumber(arr1, number))
-        */
-
-        //printArray(arr3);
-        //distinctArray(arr1,arr2,arr3);
+        System.out.println("The max number with the length " + number + " is " + maxNumber(arr1, number));
         arr3 = distinctArray(arr1, arr2, ARRAY_SIZE);
         printArray(arr3);
         System.out.println("The reverse number is " + buildReverseNumber(arr3));
@@ -41,8 +37,7 @@ public class Practice1 {
     }
 
     public static int maxNumber(int[] arr, int numberLength) {
-        int maxNumber = 0;
-        int maxDigit = arr[0], tempMax;
+        int maxNumber = 0, tempMax;
         for (int index = 0; index <= arr.length - numberLength; index++) {
             tempMax = buildNumber(arr, index, numberLength);
             if (maxNumber < tempMax) {
@@ -61,20 +56,18 @@ public class Practice1 {
         return number;
     }
 
-    public static int buildReverseNumber(int[] arr){
-        int reverseNumber=0, place=1, index=arr.length-1;
-        while (arr[index]<0){
-            index--;
-        }
-        for (int digit = index; digit >=0 ; digit--) {
-            reverseNumber=reverseNumber*10+arr[digit];
-            place*=10;
+    public static int buildReverseNumber(int[] arr) {
+        int reverseNumber = 0, place = 1, index = 0;
+        while (arr[index] != -1) {
+            reverseNumber += arr[index] * place;
+            place *= 10;
+            index++;
         }
         return reverseNumber;
     }
 
-    public static int[] fillMinus(int size){
-        int[] arr=new int[size];
+    public static int[] fillMinus(int size) {
+        int[] arr = new int[size];
         for (int index = 0; index < arr.length; index++) {
             arr[index] = -1;
         }
@@ -82,30 +75,22 @@ public class Practice1 {
     }
 
     public static int[] distinctArray(int[] arr1, int[] arr2, int size) {
-        int[] distinct=fillMinus(size);
-        boolean hasBeen, isDouble;
+        int[] distinct = fillMinus(size);
         int place = 0;
-        for (int index1 = 0; index1 < arr1.length; index1++) {
-            hasBeen = false;
-            for (int index2 = 0; index2 < arr2.length; index2++) {
-                if (arr1[index1] == arr2[index2]) {
-                    hasBeen = true;
-                    break;
-                }
-            }
-            if (!hasBeen) {
-                isDouble = false;
-                for (int index3 = 0; index3 <= place; index3++) {
-                    if (arr1[index1] == distinct[index3]) {
-                        isDouble = true;
-                        break;
-                    }
-                }
-                if (!isDouble) {
-                    distinct[place++] = arr1[index1];
-                }
+        for (int index = 0; index < arr1.length; index++) {
+            if (!hasBeen(arr2, arr1[index]) && !hasBeen(distinct, arr1[index])) {
+                distinct[place++] = arr1[index];
             }
         }
         return distinct;
+    }
+
+    public static boolean hasBeen(int[] arr, int number) {
+        for (int index = 0; index < arr.length; index++) {
+            if (arr[index] == number) {
+                return true;
+            }
+        }
+        return false;
     }
 }
